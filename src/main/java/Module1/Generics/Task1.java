9bcad1f2020b721cff24462e5eef17d1214fbc26
CkavdiaRI <chirag.kavdia_1@scaler.com>
2025-12-03 07:20:14 +0530

    Task 1

---
+++ src/main/java/Module1/Generics/Task1.java
@@ -0,0 +1,43 @@
+package Module1.Generics;
+
+//Milestone 1: Item Class Hierarchy
+//
+//Objective: Create a class hierarchy to represent different item types.
+//
+//Tasks:
+//
+//Create Base Class Item:
+//
+//Define common attributes:
+//
+    //String id (unique identifier).
+    //String name (name of the item).
+    //
+    //double price (price of the item).
+    //
+    //int quantity (quantity in stock).
+    //
+    //Implement constructors and getter/setter methods.
+
+//Create Subclasses for Specific Item Types:
+        //
+        //Electronics: Add int warranty attribute.
+        //
+//        Clothing: Add String size attribute.
+        //
+        //Book: Add String author attribute.
+        //
+//Implement Comparable<Item> Interface:
+//
+//Learning Resource: Comparable Interface in Java with Examples - Scaler Topics
+//
+//Write a compareTo method to sort items alphabetically by name.
+//
+//Write a Test Program for Subclasses:
+//
+//Instantiate a few items for each subclass.
+//Add them to a list and test sorting by price.
+
+
+public class Task1 {
+}
